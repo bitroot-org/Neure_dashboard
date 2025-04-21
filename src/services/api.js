b9cd0c0ac403189c4cf630aeb20c80110d40c17d
CHANDAN YADAV <87558531+CHANDAN2901@users.noreply.github.com>
2025-04-21 18:19:49 +0530

    base url updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-console.log('API Base URL:', import.meta.env.VITE_API_URL);
+console.log('API Base URL:', import.meta.env.VITE_API_BASE_URL);
 
 
 const api = axios.create({
